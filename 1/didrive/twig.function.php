ca43b76a79d311016fd77c15ca6cd68dfb38d6cd
Сергей <nyos@rambler.ru>
2019-11-04 13:38:50 +0500

    no message

--- 1/didrive/twig.function.php
+++ 1/didrive/twig.function.php
@@ -8,8 +8,6 @@
 
 $function = new Twig_SimpleFunction('iiko_oborot__get_oborots_on_sp', function ( $db, string $sp, string $date_start, string $date_finish ) {
 
-
-
 //    \Nyos\mod\items::$sql_itemsdop_add_where_array = array(
 //        ':sp' => $sp
 //    );
@@ -20,17 +18,27 @@ $function = new Twig_SimpleFunction('iiko_oborot__get_oborots_on_sp', function (
 //                AND md1.name = \'sale_point\'
 //                AND md1.value = :sp
 //        ';    
-    $oborots0 = \Nyos\mod\items::getItemsSimple($db, 'sale_point_oborot');
-    // \f\pa($oborots);
+    $oborots = \Nyos\mod\items::getItemsSimple($db, 'sale_point_oborot');
+//    echo '<pre>';
+//    \f\pa($oborots,2);
+//    echo '</pre>';
+    
+    $re = [ 'summa' => 0 ];
 
-    $re = [];
-
-    foreach ($oborots0['data'] as $k => $v) {
-        if ( isset($v['dop']['sp']) && isset($v['dop']['date']) && isset($v['dop']['sale_point']) && $v['dop']['sale_point'] == $sp ) {
+    foreach ($oborots['data'] as $k => $v) {
+        if ( 
+                isset($v['dop']['sale_point']) && 
+                $v['dop']['sale_point'] == $sp &&
+                isset($v['dop']['date']) && 
+                $v['dop']['date'] >= $date_start && 
+                $v['dop']['date'] <= $date_finish                
+                ) {
 
             $re[$v['dop']['date']] = $v['dop'];
             $re[$v['dop']['date']]['id'] = $v['id'];
-
+            
+            $re['summa'] += $v['dop']['oborot_server'];
+            
         }
     }
 
